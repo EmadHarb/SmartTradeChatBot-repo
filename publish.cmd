e00947f01f20a8bab12d88cd0c21d06a7d45eb69
EmadHarb <EmadHarb@users.noreply.github.com>
2019-10-08 07:29:40 +0100

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=smarttradechatbot-Web-Deploy.pubxml -p:Password=3ovEektt4Mfxij1L0bK81oC0GQYNqaan7pxsjv1klPQW7SDmDxGbsSG68Pvb
+
