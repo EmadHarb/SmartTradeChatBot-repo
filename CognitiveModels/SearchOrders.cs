9eae13a65693ac68ec72fcf121ed0a5b08c3d00b
EmadHarb <EmadHarb@users.noreply.github.com>
2019-11-10 22:13:46 +0000

    SearchOrder

--- CognitiveModels/SearchOrders.cs
+++ CognitiveModels/SearchOrders.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.Bot.Builder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CoreBot.CognitiveModels
 {
-    public class SearchOrders
+    public class SearchOrders : IRecognizerConvert
     {
+        public void Convert(dynamic result)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
